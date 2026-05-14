0a648478f06ea042d9326d81fade24d6bfd61562
Piyush <piyushjethani225@gmail.com>
2026-05-14 20:01:41 +0530

    frontend update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Hero from '@/components/Hero'
 import Features from '@/components/Features'
-import Footer from '@/components/Footer'
 
 const Home = () => {
   return (
